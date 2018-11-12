53fb61da598e6f5071bf2cd75a85902a17bae81c
Piotr Lelito <piotr.lelito@vsoft.pl>
2018-11-12 09:07:37 +0100

    Configure local builds

--- build/build-mvc.ps1
+++ build/build-mvc.ps1
@@ -1,37 +1,37 @@
-# COMMON PATHS
-
+# VARIABLES
+$appPoolName = "EM"
 $buildFolder = (Get-Item -Path "./" -Verbose).FullName
 $slnFolder = Join-Path $buildFolder "../"
 $outputFolder = Join-Path $buildFolder "outputs"
 $webMvcFolder = Join-Path $slnFolder "src/EM.Web.Mvc"
 
-## CLEAR ######################################################################
+## IIS STOP
+Stop-WebAppPool -Name $appPoolName
 
+## CLEAR
 Remove-Item $outputFolder -Force -Recurse -ErrorAction Ignore
 New-Item -Path $outputFolder -ItemType Directory
 
-## RESTORE NUGET PACKAGES #####################################################
-
+## RESTORE NUGET PACKAGES
 Set-Location $slnFolder
 dotnet restore
 
-## PUBLISH WEB MVC PROJECT ###################################################
-
+## PUBLISH WEB MVC PROJECT
 Set-Location $webMvcFolder
-dotnet publish --output (Join-Path $outputFolder "Mvc")
+dotnet publish -c Debug --output (Join-Path $outputFolder "Mvc")
+#dotnet publish -c Release -r win10-x64 --self-contained --output (Join-Path $outputFolder "Mvc")
 
-## CREATE DOCKER IMAGES #######################################################
+## CREATE DOCKER IMAGES
 
 # Mvc
-Set-Location (Join-Path $outputFolder "Mvc")
+#Set-Location (Join-Path $outputFolder "Mvc")
 
-docker rmi abp/mvc -f
-docker build -t abp/mvc .
+#docker rmi abp/mvc -f
+#docker build -t abp/mvc .
 
-## DOCKER COMPOSE FILES #######################################################
+## DOCKER COMPOSE FILES
+#Copy-Item (Join-Path $slnFolder "docker/mvc/*.*") $outputFolder
 
-Copy-Item (Join-Path $slnFolder "docker/mvc/*.*") $outputFolder
-
-## FINALIZE ###################################################################
-
-Set-Location $outputFolder
+## FINALIZE
+Start-WebAppPool -Name $appPoolName
+Set-Location $buildFolder
